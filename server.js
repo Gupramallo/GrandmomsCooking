3c5d608540e664d6fb01d0414205ded230d92f40
Wololo <46468290+wololo-aeyoyo@users.noreply.github.com>
2021-08-17 17:01:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const app = express();
-app.use(requireHTTPS);
 
+app.use(express.static(__dirname+'/dist/ng-blog'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+});
 
-app.use(express.static('./dist/cooking'));
-
-
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/cooking/'}
-  );
-  });
-
-
-  app.listen(process.env.PORT || 3700);
+app.listen(process.env.PORT || 8080);
